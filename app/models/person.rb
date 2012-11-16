943f0cd55907b0ffc0e2c5d40164acb7cbf48934
Ace Atienza <ace@ace.com>
2012-11-16 00:37:37 -0500

    add validations and a little css

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,11 @@
 class Person < ActiveRecord::Base
   attr_accessible :birthday, :body_temperature, :can_send_email, :country,
 :description, :email, :favorite_time, :graduation_year, :name, :price, :secret
+
+	validates_presence_of :name,
+		:message => "must be provided. I won't tell anyone else."
+	validates_length_of :name, :in => 3..24
+	validates_inclusion_of :country,
+		:in => ['Canada', 'US'],
+		:message => "must be Canada or US"
 end
